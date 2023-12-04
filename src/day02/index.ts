771cbef68b05d580d75ba44ab30adb186ce46fcd
Henrique Gonçalves <henrique@devsquad.com>
2023-12-04 18:27:26 -0300

    Adjust duplicate test inputs

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -52,17 +52,19 @@ const part2 = (rawInput: string) => {
   }, 0);
 };
 
+const testInput = `
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`;
+
 run({
   part1: {
     tests: [
       {
-        input: `
-          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-        `,
+        input: testInput,
         expected: 8,
       },
     ],
@@ -71,13 +73,7 @@ run({
   part2: {
     tests: [
       {
-        input: `
-          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-        `,
+        input: testInput,
         expected: 2286,
       },
     ],
